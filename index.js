ac0a0a84f8091498b36cd2bbcb33934781a31d1c
Madeline Guman <mads890@yahoo.com>
2020-03-25 13:23:39 -0400

    empty attempt #2

--- index.js
+++ index.js
@@ -30,10 +30,9 @@ function getParks(place, numResults) {
 
 function displayParks(responseJson) {
     $('.results').removeClass('hidden');
-    $('.resultslist').empty();
     console.log(responseJson)
     for (let i = 0; i < responseJson.data.length; i++) { 
-         $('.results').append(`<li><h2>${responseJson.data[i].name}</h2><p>${responseJson.data[i].description}</p><p>More info at <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p></li>`)
+         $('.resultslist').append(`<li><h2>${responseJson.data[i].name}</h2><p>${responseJson.data[i].description}</p><p>More info at <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p></li>`)
         }
 }
 
@@ -46,7 +45,7 @@ function formSubmit() {
     $('form').submit(event => {
         event.preventDefault();
         $('.results').addClass('hidden');
-        
+        $('resultslist').empty();
         $('.error').empty().addClass('hidden');
         let state = $('#parksearch').val();
         let parkNum = $('#maxnum').val();
